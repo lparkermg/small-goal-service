67b2236b017dd2d1393db1c97a914078e5d4b91b
Luke Parker <lparker@moongate-uk.co.uk>
2015-11-21 15:31:32 +0200

    Initial Stuff.

---
+++ src/DataAccess/FileIO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataAccess
+{
+	public class FileIO
+	{
+		private string _filePath;
+		public FileIO (string filePath)
+		{
+			_filePath = filePath;
+		}
+
+		public List<string> LoadActivities (){
+			
+		}
+
+		public Dictionary<int,int> LoadMinMax(string activity){
+			
+		}
+
+		public void CheckFileExists(){
+			
+		}
+
+		public bool IsFileValid(){
+			
+		}
+
+		private void CreateSettingsFile(){
+			
+		}
+	}
+}
+
